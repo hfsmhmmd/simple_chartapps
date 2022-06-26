d815adad845f1c5021d9a3c37743a5f728487b82
Muhammad Hafiz <21hafizmuhammad@gmail.com>
2022-06-27 03:19:11 +0700

    Update on main.js
    create execute function to seperate functional function to be executable independently

--- main.js
+++ main.js
@@ -1,45 +1,54 @@
 document.getElementById('Form').onsubmit = submit;
-function submit(e){      
-        const input = document.getElementById("inp").value;
-        console.log(input);
-        var theArr=[];   
-        var bgColor=[];       
-        for (let iter = 0; iter < input ;iter++){
-    
-            var dataRandom =  Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-            theArr.push(dataRandom);
+function submit(e){  
+        executeChart();
+        e.preventDefault();       
         }
-        for (let iter = 0; iter < input ;iter++){
+function executeChart(){
+    const input = document.getElementById("inp").value;
+    var arrayData=[];   
+    var bgColor=[];       
+    arrayData = generateRandom(input)
+    bgColor = generateBarColor(input,arrayData)
+    generateChart(arrayData,bgColor);
+}
+function generateRandom(amount){
+    var randomCollection=[]
+    for (let iter = 0; iter < amount ;iter++){
+        var dataRandom =  Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+      randomCollection.push(dataRandom);   
+    }
+    return(randomCollection);
+}
+function generateBarColor(input,theArr){
+    var bgColor=[];
+    for (let iter = 0; iter < input ;iter++){
     
-            if(theArr[iter]<25){{bgColor.push('green')}}
-            if(theArr[iter]>= 26 && theArr[iter]<51){{bgColor.push('green')}}
-            if(theArr[iter]>= 51 && theArr[iter]<76){{bgColor.push('red')}}
-            if(theArr[iter]>76){{bgColor.push('blue')}}
-        }    
-       
-        generateChart(theArr,bgColor)
-        e.preventDefault();
-        }
-
+        if(theArr[iter]<25){{bgColor.push('green')}}
+        if(theArr[iter]>= 26 && theArr[iter]<51){{bgColor.push('green')}}
+        if(theArr[iter]>= 51 && theArr[iter]<76){{bgColor.push('red')}}
+        if(theArr[iter]>76){{bgColor.push('blue')}}
+    }   
+    return(bgColor);
+}
 function generateChart(floor,bgColor){ 
     const ctx = document.getElementById('cookieChart');
-    const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: floor,
-            datasets: [{
-                label: 'Charts',
-                data: floor,
-                backgroundColor: bgColor,
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: floor,
+                datasets: [{
+                    label: 'Charts',
+                    data: floor,
+                    backgroundColor: bgColor,
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
                 }
             }
-        }
-
-    });
+    
+        });   
 }
